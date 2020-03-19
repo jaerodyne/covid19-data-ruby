fd2a27b74ba624c4266d9611da8c8e8ad27d99f0
jaerodyne <jsomera@gmail.com>
2020-03-18 21:43:21 -0500

    Initialize gem and write wrapper

---
+++ covid19_data_ruby.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s| 
+  ### REQUIRED ###
+  s.name        = 'covid19_data_ruby'
+  s.version     = '0.0.0'
+  s.summary     = "This is Covid19Data"
+  s.authors     = ["Jillian Somera"]
+  s.files       = ["lib/covid19_data_ruby.rb"]
+### WARNING ###
+  s.homepage    = 'https://github.com/jaerodyne/covid19_data_ruby'
+  s.license     = 'MIT' 
+### RECOMMENDED ###
+  s.description = "A Ruby wrapper for the coronavirus API at https://github.com/ExpDev07/coronavirus-tracker-api" # nice to have 
+end
---
+++ covid19_data_ruby.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+
+class Covid19Data < ActiveRecord::Base
+  BASE_URL = 'https://coronavirus-tracker-api.herokuapp.com/v2/'
+
+  class << self
+    def get_latest
+      uri = URI(BASE_URL + 'latest')
+      get_json_data(uri)
+    end
+  
+    def get_all_locations
+      uri = URI(BASE_URL + 'locations')
+      get_json_data(uri)
+    end
+    
+      
+    def find_by_country_code(country_code, with_timelines=false)
+      timelines = with_timelines ? '&timelines=true' : ''
+      uri = URI("#{BASE_URL}locations?country_code=#{country_code.upcase}#{timelines}")
+      get_json_data(uri)
+    end
+
+    def find_by_location(id)
+      uri = URI("#{BASE_URL}locations/#{id}")
+      get_json_data(uri)
+    end
+    
+    private
+
+    def get_json_data(uri)
+      response = Net::HTTP.get(uri)
+      JSON.parse(response)
+    end
+  end
+end
